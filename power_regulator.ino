546d7c175f3ad923457305c0f2f4e7969bfe4f25
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2023-05-31 11:12:36 +0200

    Arduino sketch formatting updated

--- power_regulator.ino
+++ power_regulator.ino
@@ -34,63 +34,63 @@ double k_d = 1.14;
 PID myPID(&current_temperature, &power, &goal_temperature, k_p, k_i, k_d, DIRECT);
 
 void setup()   {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  display.begin();
-  display.setContrast(30); 
-  
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
-  pinMode(12, INPUT); 
-  
-  Timer1.initialize(100000); //us
-  Timer1.pwm(9, 0);
-  Timer1.attachInterrupt(timer1Callback);
+    display.begin();
+    display.setContrast(30);
 
-  myPID.SetOutputLimits(0., 100.);
-  myPID.SetMode(AUTOMATIC);
+    pinMode(10, INPUT);
+    pinMode(11, INPUT);
+    pinMode(12, INPUT);
+
+    Timer1.initialize(100000); //us
+    Timer1.pwm(9, 0);
+    Timer1.attachInterrupt(timer1Callback);
+
+    myPID.SetOutputLimits(0., 100.);
+    myPID.SetMode(AUTOMATIC);
 }
 
 void lcdPrintString(String s) {
-  for (unsigned i = 0; i < s.length(); ++i) {
-    display.write(s[i]);
-  }
+    for (unsigned i = 0; i < s.length(); ++i) {
+        display.write(s[i]);
+    }
 }
 
 void timer1Callback() {
-  Timer1.pwm(9, int(power / 100. * 1023.));
+    Timer1.pwm(9, int(power / 100. * 1023.));
 }
 
 void loop() 
 {
-  if (millis() - buttonPressedTimestamp > buttonTimeoutMs) {
-    if (digitalRead(10) == HIGH && goal_temperature > minGoalTemperature) {
-      goal_temperature -= 1;
-      buttonPressedTimestamp = millis();
+    if (millis() - buttonPressedTimestamp > buttonTimeoutMs) {
+        if (digitalRead(10) == HIGH && goal_temperature > minGoalTemperature) {
+            goal_temperature -= 1;
+            buttonPressedTimestamp = millis();
+        }
+        if (digitalRead(12) == HIGH && goal_temperature < maxGoalTemperature) {
+            goal_temperature += 1;
+            buttonPressedTimestamp = millis();
+        }
     }
-    if (digitalRead(12) == HIGH && goal_temperature < maxGoalTemperature) {
-      goal_temperature += 1;
-      buttonPressedTimestamp = millis();
+
+    if (millis() - adcTimestamp > adcTimeoutMs) {
+        current_temperature = thermocouple.readCelsius();
+        myPID.Compute();
+        adcTimestamp = millis();
     }
-  }
-  
-  if (millis() - adcTimestamp > adcTimeoutMs) {
-    current_temperature = thermocouple.readCelsius();
-    myPID.Compute();
-    adcTimestamp = millis();
-  }
-  
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextColor(BLACK);
-  display.setTextSize(1);
-  int line = 0;
-  display.setCursor(0, line++ * 10);
-  lcdPrintString("T_trgt: " + String(goal_temperature, 1) + "C");
-  display.setCursor(0, line++ * 10);
-  lcdPrintString("T_crnt: " + String(current_temperature, 1) + "C");
-  display.setCursor(0, line++ * 10);
-  lcdPrintString("P: " + String(power, 1) + "%");
-  display.display();
+
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextColor(BLACK);
+    display.setTextSize(1);
+    int line = 0;
+    display.setCursor(0, line++ * 10);
+    lcdPrintString("T_trgt: " + String(goal_temperature, 1) + "C");
+    display.setCursor(0, line++ * 10);
+    lcdPrintString("T_crnt: " + String(current_temperature, 1) + "C");
+    display.setCursor(0, line++ * 10);
+    lcdPrintString("P: " + String(power, 1) + "%");
+    display.display();
 }
 
